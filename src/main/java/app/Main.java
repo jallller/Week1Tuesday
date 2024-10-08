37bd99e9385a3f2c0d582e27e49226a620fa4e42
Jantie Larsen <jantielarsen@gmail.com>
2024-10-08 19:34:49 +0200

    transaction class + methods

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 public abstract class Main {
     public static void main(String[] args) {
+/*
         Book[] sampleBooks = {
                 new Book("The Catcher in the Rye", "J.D. Salinger", 1951, 277, 4.1),
                 new Book("To Kill a Mockingbird", "Harper Lee", 1960, 281, 4.3),
@@ -52,30 +53,98 @@ public abstract class Main {
 
         //Calculate the total number of pages for all books (assuming each book has a fixed number of pages).
         totalNrPages(books);
+*/
 
 
+        List<Transaction> transactions = new ArrayList<>();
 
-//        List<Transaction> transactions = new ArrayList<>();
-//
-//        transactions.add(new Transaction(1, 150.75, "USD"));
-//        transactions.add(new Transaction(2, 200.00, "EUR"));
-//        transactions.add(new Transaction(3, 300.50, "USD"));
-//        transactions.add(new Transaction(4, 400.25, "GBP"));
-//        transactions.add(new Transaction(5, 100.00, "JPY"));
-//        transactions.add(new Transaction(6, 250.00, "USD"));
-//        transactions.add(new Transaction(7, 350.75, "EUR"));
-//        transactions.add(new Transaction(8, 450.00, "CAD"));
-//
-//        // Print the sample transactions
-//        for (Transaction transaction : transactions) {
-//            System.out.println(transaction);
-//        }
+        transactions.add(new Transaction(1, 150.75, "USD"));
+        transactions.add(new Transaction(2, 200.00, "EUR"));
+        transactions.add(new Transaction(3, 300.50, "USD"));
+        transactions.add(new Transaction(4, 400.25, "GBP"));
+        transactions.add(new Transaction(5, 100.00, "JPY"));
+        transactions.add(new Transaction(6, 250.00, "USD"));
+        transactions.add(new Transaction(7, 350.75, "EUR"));
+        transactions.add(new Transaction(8, 450.00, "CAD"));
 
+        // Print the sample transactions
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction);
+        }
+
+
+        //Calculate the total sum of all transaction amounts
+        totalSumTransactionAmount(transactions);
+
+        //Group transactions by currency and calculate the sum of amounts for each currency.
+        groupSumCurAmounts(transactions);
+
+        //Find the highest transaction amount.
+        highestTransaction(transactions);
+
+        //Find the average transaction amount.
+        avgTransactionAmount(transactions);
+
+    }
+
+
+    //Calculate the total sum of all transaction amounts
+
+    public static double totalSumTransactionAmount(List<Transaction> transactions) {
+        double totalAmount = transactions.stream()
+                .collect(Collectors.summingDouble(Transaction::getAmount));  // Summing up transaction amounts
+
+        System.out.println("Total transaction amount: " + totalAmount);
+        return totalAmount;
+    }
+
+    //Group transactions by currency and calculate the sum of amounts for each currency.
+    public static void groupSumCurAmounts(List<Transaction> transaction) {
+        transaction.stream()
+                .collect(Collectors.groupingBy(
+                        Transaction::getCurrency,
+                        Collectors.summingDouble(Transaction::getAmount)
+                ))
+                .forEach((currency,sumAmount) ->
+                        System.out.println("Currency: " + currency + "amount :" + sumAmount));
+    }
+
+    //Find the highest transaction amount.
+    public static void highestTransaction(List<Transaction> transactions){
+        transactions.stream()
+                .max(Comparator.comparing(Transaction::getAmount))
+                .ifPresent(transaction -> System.out.println("Highest amount: " + transaction.getAmount()));
+
+
+    }
+
+    //Find the average transaction amount.
+
+    public static void avgTransactionAmount(List<Transaction> transactions){
+        double avgTransaction = transactions.stream()
+                .collect(Collectors.averagingDouble(Transaction::getAmount));  // Calculate the average rating
+
+        System.out.println("Avg transaction : " + avgTransaction);
+
+    }
+
+
+    //Group books by author and calculate the average rating for each author’s books.
+    public static void groupByAutherAvgRating(List<Book> books) {
+        books.stream()
+                .collect(Collectors.groupingBy(
+                        Book::getAuther,
+                        Collectors.averagingDouble(Book::getRating)
+                ))
+                .forEach((auther, avgRating) ->
+                        System.out.println("Auther: " + auther + ", avg rating: " + avgRating));
     }
 
 
 
 
+
+/*
     //Filter and display books published after a specific year.
     public static void booksPublishedAfter(int year, List<Book> books) {
         System.out.println("\nBooks published after " + year + ":");
@@ -119,8 +188,8 @@ public abstract class Main {
                 .sum();
         System.out.println("number of pages : " + pages);
         return pages;
+//}
 
-    }
 
     //Find the average rating of all books.
 
@@ -129,8 +198,7 @@ public abstract class Main {
                 .collect(Collectors.averagingDouble(Book::getRating));  // Calculate the average rating
 
         System.out.println("Avg rating all : " + avgRating);
-    }
-
+    }*/
 
 
 }
